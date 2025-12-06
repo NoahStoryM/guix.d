8a8b7f981fe0054b5880a3d6996c774b5b496087
NoahStoryM <noahstorym@gmail.com>
2025-12-06 20:55:51 +0800

    Set commit to `guix` and `nonguix` channels

--- channels.scm
+++ channels.scm
@@ -6,6 +6,7 @@
  (channel
   [name 'nonguix]
   [url "https://gitlab.com/nonguix/nonguix"]
+  [commit "d741f41eca0a04694775118eedaa332ffa1a26f8"]
   [introduction
    (make-channel-introduction
     "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
@@ -13,5 +14,6 @@
      "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))])
  (channel
   [inherit (car %default-channels)]
-  [url "https://mirror.sjtu.edu.cn/git/guix.git"])
+  [url "https://mirror.sjtu.edu.cn/git/guix.git"]
+  [commit "32edabdabd58e74edd7b57774fea977084cdf826"])
  (cdr %default-channels))
