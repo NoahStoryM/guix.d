d916c9838b8882576ec0f3f9d2219c76f1da4243
NoahStoryM <noahstorym@gmail.com>
2025-12-06 11:58:33 +0800

    First commit

---
+++ system/config.scm
@@ -0,0 +1,81 @@
+;; This is an operating system configuration generated
+;; by the graphical installer.
+;;
+;; Once installation is complete, you can learn and modify
+;; this file to tweak the system configuration, and pass it
+;; to the 'guix system reconfigure' command to effect your
+;; changes.
+
+
+;; Indicate which modules to import to access the variables
+;; used in this configuration.
+(use-modules (gnu)
+             (gnu system locale)
+             (nongnu packages linux)
+             (nongnu system linux-initrd))
+(use-service-modules cups desktop networking ssh xorg)
+
+(operating-system
+  [locale "en_US.utf8"]
+  [locale-definitions
+   (cons*
+    (locale-definition [name "zh_CN.utf8"] [source "zh_CN"])
+    %default-locale-definitions)]
+  [timezone "Asia/Shanghai"]
+  [keyboard-layout
+   (keyboard-layout
+    "us"
+    #:options '("ctrl:swapcaps" "parens:swap_brackets"))]
+  [host-name "x1c18"]
+
+  ;; Nonguix
+  [kernel linux]
+  [initrd microcode-initrd]
+  [firmware (list linux-firmware)]
+
+  ;; The list of user accounts ('root' is implicit).
+  [users (cons* (user-account
+                 [name "noah"]
+                 [comment "Noah"]
+                 [group "users"]
+                 [home-directory "/home/noah"]
+                 [supplementary-groups '("wheel" "netdev" "audio" "video")])
+                %base-user-accounts)]
+
+  ;; Packages installed system-wide.  Users can also install packages
+  ;; under their own account: use 'guix search KEYWORD' to search
+  ;; for packages and 'guix install PACKAGE' to install a package.
+  [packages (append (specifications->packages '("niri" "fuzzel" "alacritty" "swaylock"))
+                    %base-packages)]
+
+  ;; Below is the list of system services.  To search for available
+  ;; services, run 'guix system search KEYWORD' in a terminal.
+  [services (cons*
+             ;; To configure OpenSSH, pass an 'openssh-configuration'
+             ;; record as a second argument to 'service' below.
+             (service openssh-service-type)
+             (set-xorg-configuration
+              (xorg-configuration [keyboard-layout keyboard-layout]))
+             %desktop-services)]
+  [bootloader (bootloader-configuration
+               [bootloader grub-efi-bootloader]
+               [targets (list "/boot/efi")]
+               [keyboard-layout keyboard-layout])]
+  [mapped-devices (list (mapped-device
+                         [source (uuid "f2a01d64-40c9-4bda-b687-e44a8bbb071c")]
+                         [target "cryptroot"]
+                         [type luks-device-mapping]))]
+
+  ;; The list of file systems that get "mounted".  The unique
+  ;; file system identifiers there ("UUIDs") can be obtained
+  ;; by running 'blkid' in a terminal.
+  [file-systems (cons* (file-system
+                         [mount-point "/boot/efi"]
+                         [device (uuid "867A-F201" 'fat32)]
+                         [type "vfat"])
+                       (file-system
+                         [mount-point "/"]
+                         [device "/dev/mapper/cryptroot"]
+                         [type "ext4"]
+                         [dependencies mapped-devices])
+                       %base-file-systems)])
---
+++ user/root/config.scm
@@ -0,0 +1,53 @@
+;; This "home-environment" file can be passed to 'guix home reconfigure'
+;; to reproduce the content of your profile.  This is "symbolic": it only
+;; specifies package names.  To reproduce the exact same profile, you also
+;; need to capture the channels being used, as returned by "guix describe".
+;; See the "Replicating Guix" section in the manual.
+
+(use-modules (gnu home)
+             (gnu packages)
+             (gnu services)
+             (guix gexp)
+             (gnu home services shells))
+
+(home-environment
+ ;; Below is the list of packages that will show up in your
+ ;; Home profile, under ~/.guix-home/profile.
+ [packages
+  (specifications->packages
+   '(;; Development Tools
+     "racket-minimal"
+     "chez-scheme"
+     "zuo"
+     "git"
+
+     ;; Text Editors
+     "emacs"
+     "neovim"
+     "vim"
+
+     ;; System Utilities
+     "curl"
+     "trash-cli"
+     "unzip"
+     "zip"
+     "iptables"
+     "blesh"))]
+
+ ;; Below is the list of Home services.  To search for available
+ ;; services, run 'guix home search KEYWORD' in a terminal.
+ [services
+  (append (list (service home-bash-service-type
+                         (home-bash-configuration
+                          [aliases '(["grep" . "grep --color=auto"]
+                                     ["ip" . "ip -color=auto"]
+                                     ["ll" . "ls -l"]
+                                     ["ls" . "ls -p --color=auto"]
+                                     ["ble" . "source ~/.guix-home/profile/share/blesh/ble.sh"])]
+                          [bashrc (list (local-file
+                                         "/home/noah/.guix.d/user/root//.bashrc"
+                                         "bashrc"))]
+                          [bash-profile (list (local-file
+                                               "/home/noah/.guix.d/user/root//.bash_profile"
+                                               "bash_profile"))])))
+          %base-home-services)])
