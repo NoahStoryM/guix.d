fad3f3525f97c3760ffe6659a399a26084b8087a
NoahStoryM <noahstorym@gmail.com>
2025-12-07 22:27:06 +0800

    Using channels to store channels

---
+++ channels.scm
@@ -0,0 +1,20 @@
+(cons*
+ (channel
+  [name 'private]
+  [url "file:///home/noah/guix.d"]
+  [branch "x1c6th"])
+ (channel
+  [name 'nonguix]
+  [url "https://gitlab.com/nonguix/nonguix"]
+  [branch "master"]
+  [commit "d741f41eca0a04694775118eedaa332ffa1a26f8"]
+  [introduction
+   (make-channel-introduction
+    "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+    (openpgp-fingerprint
+     "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))])
+ (channel
+  [inherit (car %default-channels)]
+  [url "https://mirror.sjtu.edu.cn/git/guix.git"]
+  [commit "32edabdabd58e74edd7b57774fea977084cdf826"])
+ (cdr %default-channels))
--- user/root/config.scm
+++ user/root/config.scm
@@ -49,27 +49,7 @@
  ;; services, run 'guix home search KEYWORD' in a terminal.
  [services
   (cons*
-   (service home-channels-service-type
-            (cons*
-             (channel
-              [name 'private]
-              [url "file:///home/noah/guix.d"]
-              [branch "x1c6th"])
-             (channel
-              [name 'nonguix]
-              [url "https://gitlab.com/nonguix/nonguix"]
-              [branch "master"]
-              [commit "d741f41eca0a04694775118eedaa332ffa1a26f8"]
-              [introduction
-               (make-channel-introduction
-                "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-                (openpgp-fingerprint
-                 "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))])
-             (channel
-              [inherit (car %default-channels)]
-              [url "https://mirror.sjtu.edu.cn/git/guix.git"]
-              [commit "32edabdabd58e74edd7b57774fea977084cdf826"])
-             (cdr %default-channels)))
+   (service home-channels-service-type (include "../../channels.scm"))
    (service home-bash-service-type
             (home-bash-configuration
              [aliases '(["grep" . "grep --color=auto"]
